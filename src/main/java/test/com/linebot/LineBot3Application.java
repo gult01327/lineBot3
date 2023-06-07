8ef99ee124ce75cbb94db0f4d1fa195f0aa698e3
gult01327 <gult01327@gmail.com>
2023-06-07 14:02:14 +0800

    test

---
+++ src/main/java/test/com/linebot/LineBot3Application.java
@@ -0,0 +1,24 @@
+package test.com.linebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RequestMapping("/robot")
+@RestController
+public class LineBot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBot3Application.class, args);
+	}
+	@GetMapping("/test")
+	public ResponseEntity<String> test() {
+		return new ResponseEntity<String>("Hello FARMER", HttpStatus.OK);
+	}
+
+}
---
+++ src/test/java/test/com/linebot/LineBot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package test.com.linebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineBot3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

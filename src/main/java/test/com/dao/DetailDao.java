5ba873c32b383d632f2d5e2e839e7d90f40e8155
gult01327 <gult01327@gmail.com>
2023-06-15 02:09:58 +0800

    移除import

--- src/main/java/test/com/dao/DetailDao.java
+++ src/main/java/test/com/dao/DetailDao.java
@@ -3,9 +3,6 @@ package test.com.dao;
 import java.util.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import test.com.model.Detail;
--- src/main/java/test/com/service/DetailService.java
+++ src/main/java/test/com/service/DetailService.java
@@ -15,10 +15,6 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
